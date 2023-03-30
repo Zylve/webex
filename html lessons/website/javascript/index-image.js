f9a7be78156781f7840ae301f844d685be162168
Zylve <70604613+Zylve@users.noreply.github.com>
2023-03-30 14:41:26 -0700

    Images

--- html lessons/website/javascript/index-image.js
+++
@@ -1,7 +0,0 @@
-(function(window, document, undefined) {
-    window.onload = init;
-  
-    function init(){
-        document.getElementById("random-image").src = "https://picsum.photos/500";
-    }
-})(window, document, undefined);
