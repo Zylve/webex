501ac0193a86395a4ad3cc4629d7774fcccb4764
Zylve <70604613+Zylve@users.noreply.github.com>
2023-04-26 14:53:02 -0700

    course schedule

---
+++ html lessons/schedule/index.js
@@ -0,0 +1,14 @@
+(function(window, document, undefined) {  
+    window.onload = init;
+  
+    function init(){
+        const table = document.getElementById("courses");
+        const data = table.getElementsByTagName("td");
+        for(let i = 0; i < data.length; i++) {
+            var color ="#" + Math.floor(Math.random() * 16777215).toString(16);
+            data[i].style.color = color
+            console.log(data[i].style.color);
+        }
+    }
+  
+  })(window, document, undefined);
