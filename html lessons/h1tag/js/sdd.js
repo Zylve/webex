8a56fffc59dad85308af3216eecaf77e7f40868c
Zylve <70604613+Zylve@users.noreply.github.com>
2023-05-10 15:05:17 -0700

    css style

---
+++ html lessons/h1tag/js/sdd.js
@@ -0,0 +1,7 @@
+function reveal(question) {
+    if(question == "q1") {
+        alert("A variable of the Integer type.");
+    } else if(question == "q2") {
+        alert("A variable of the String type.");
+    }
+}
