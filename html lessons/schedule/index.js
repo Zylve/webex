6c79c2c608db51fe2a09c9c018a6bcf5265ac54a
Zylve <70604613+Zylve@users.noreply.github.com>
2023-05-16 14:26:02 -0700

    js for answers

--- html lessons/schedule/index.js
+++ html lessons/schedule/index.js
@@ -7,7 +7,6 @@
         for(let i = 0; i < data.length; i++) {
             var color ="#" + Math.floor(Math.random() * 16777215).toString(16);
             data[i].style.color = color
-            console.log(data[i].style.color);
         }
     }
   
