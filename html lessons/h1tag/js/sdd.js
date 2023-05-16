2a082c43dfe0293f48bfcdb2c214ce825755968a
Zylve <70604613+Zylve@users.noreply.github.com>
2023-05-16 14:26:02 -0700

    js for answers

--- html lessons/h1tag/js/sdd.js
+++ html lessons/h1tag/js/sdd.js
@@ -1,9 +1,22 @@
-function reveal(question) {
-    switch(question) {
-        case 1:
-            alert("A variable of the Integer type.");
+(function (window, document, undefined) {
+    window.onload = init;
 
-        case 2:
-            alert("A variable of the String type.");
+    function init() {
+        let answers = document.getElementsByClassName("answer");
+
+        for(let i = 0; i < answers.length; i++) {
+            answers[i].style.display = "none";
+        }
+    }
+
+})(window, document, undefined);
+
+function reveal(element, question) {
+    if (element.innerHTML == "Reveal Answer") {
+        element.innerHTML = "Hide Answer";
+        document.getElementById("q" + question + "a").style.display = "block";
+    } else {
+        element.innerHTML = "Reveal Answer";
+        document.getElementById("q" + question + "a").style.display = "none";
     }
 }
