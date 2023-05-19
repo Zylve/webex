d1d3b9e0f23314a88af1856dc8aa6bb10e9c684f
Zylve <70604613+Zylve@users.noreply.github.com>
2023-05-19 15:08:59 -0700

    Save count

--- src/components/NumberCounter.vue
+++ src/components/NumberCounter.vue
@@ -1,16 +1,27 @@
 <script setup>
 
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const count = ref(0);
 
 function increment() {
     count.value++;
+    save();
 }
 
 function decrement() {
     count.value--;
+    save();
 }
+
+function save() {
+    localStorage.setItem('count', count.value);
+}
+
+onMounted(() => {
+    const savedCount = parseInt(localStorage.getItem('count'));
+    count.value = isNaN(savedCount) ? 0 : savedCount;
+})
 
 </script>
 
