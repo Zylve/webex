188bfbfd1c2496dac4083273ddbfd75858ead643
Zylve <70604613+Zylve@users.noreply.github.com>
2023-06-05 22:04:46 +0000

    skills section

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,29 @@
 <script setup>
 import InfoBlock from '../components/InfoBlock.vue';
+
+function isVisible(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top < window.innerHeight && rect.bottom >= 0;
+}
 </script>
 
 <template>
     <main>
         <div id="main-area">
             <InfoBlock id="main-block">
-                <h1>Vuk Tacic</h1>
+                <h1 id="main-block-title">Vuk Tacic</h1>
                 <p id="main-block-info">I'm a 14 year old from Vancouver, BC. Whenever I'm not learning something new you can find me reading a good book or out skiing and hiking on the mountains. I believe that there is something to be learned from everything you experience, and that self-reflection is crucial to living a successful life.</p>
                 <p id="main-block-quote">"Show me a man who has never failed and I'll show you a man who has never achieved much."</p>
             </InfoBlock>
             <img id="splash" src="@/assets/img/index/splash.jpg">
         </div>
+        <Transition name="fadein">
+            <div id="skill-area">
+                <InfoBlock id="skill-block">
+                    <h1 id="skill-block-title">Skills</h1>
+                </InfoBlock>
+            </div>
+        </Transition>
     </main>
 </template>
 
@@ -32,6 +44,8 @@ import InfoBlock from '../components/InfoBlock.vue';
         overflow: hidden;
         align-items: center;
         animation: fade-in 1.5s ease-in-out;
+
+        border-bottom: 2rem solid #443C68;
     }
 
     #main-block {
@@ -67,5 +81,45 @@ import InfoBlock from '../components/InfoBlock.vue';
         height: 100vh;
         mask-image: linear-gradient(to left, rgba(0, 0, 0, 0.667), rgba(0, 0, 0, 0));
         -webkit-mask-image: linear-gradient(to left, rgba(0, 0, 0, 0.667), rgba(0, 0, 0, 0));
+    }
+
+    #skill-area {
+        display: flex;
+        height: 100vh;
+        overflow: hidden;
+        align-items: center;
+        animation: fade-in 1.5s ease-in-out;
+    }
+
+    #skill-block {
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        margin: auto;
+        margin-top: 5vh;
+        margin-bottom: 5vh;
+
+        height: 80vh;
+        width: 80vw;
+        padding: 1rem;
+        border-radius: 1rem;
+
+        background-color: #251e3a;
+        color: white;
+    }
+
+    #skill-block-title {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .fadein-enter-active,
+    .fadein-leave-active {
+        transition: opacity 0.5s ease;
+    }
+
+    .fadein-enter-from,
+    .fadein-leave-to {
+        opacity: 0;
     }
 </style>
